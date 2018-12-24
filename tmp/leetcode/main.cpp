31f621c92417f08f8ef5c95080a822604dd50c4e
shuai.liu.dev <shuai.liu.dev@ihandysoft.com>
2018-12-24 21:40:40 +0800

    tmp

--- tmp/leetcode/main.cpp
+++ tmp/leetcode/main.cpp
@@ -7,30 +7,24 @@ int divide(int dividend, int divisor) {
 
     unsigned int dvd = dividend > 0 ? dividend : -dividend;
     unsigned int dvs = divisor > 0 ? divisor : -divisor;
+    cout << dvd << endl;
 
-    cout << "dvd=" << dvd << ", dvs" << dvs << endl;
     unsigned int array[32];
-    int i = 0;
-    unsigned int d = dvs;
+    unsigned int i = 0;
+    long long d = dvs;
     while (dvd >= d) {
         array[i] = d;
         d = d << 1;
-        cout << "i = " << i  << ", array[i]=" << array[i] << ", d = "<< d << endl;
         ++i;
     }
     long long result = 0;
     for(--i; i >= 0; --i) {
-        array[i] = dvs << i;
-        cout << "i = " << i  << ", array[i]=" << array[i] << endl;
-        if(dvd > array[i]) {
+        if(dvd >= array[i]) {
             dvd -= array[i];
             result += 1 << i;
         }
-        cout << "current result = " << result << endl;
     }
     result = sign > 0 ? result : -result;
-    cout << INT_MAX << " , result = " << result << " , "<< (result > INT_MAX) << endl;
-    cout << INT_MIN << " , result = " << result << " , "<< (result < INT_MIN) << endl;
     if(result > INT_MAX || result < INT_MIN) {
         return INT_MIN;
     }
@@ -39,7 +33,7 @@ int divide(int dividend, int divisor) {
 
 int main() {
 
-    cout << "1/1 " << divide(1, 1) << endl;
+//    cout << "1/1 " << divide(1, 1) << endl;
 
 //    cout << "0/2=" << divide(0, 2) << endl;
 //    cout << "10/2=" << divide(10, 2) << endl;
@@ -57,5 +51,7 @@ int main() {
 //    cout << "2147483647/10=" << divide(2147483647, 10) << endl;
 //    cout << "-2147483648/1=" << divide(-2147483648, 1) << endl;
 //    cout << "-2147483648/-1=" << divide(-2147483648, -1) << endl;
+    cout << "-2147483648/-1=" << divide(-2147483648, -1) << endl;
+
     return 0;
 }
