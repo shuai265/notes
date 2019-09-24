85155880ff81fb1a1576cc0ec9aa61eb70a0ff1a
shuai.liu.dev <shuai.liu.dev@ihandysoft.com>
2019-09-25 00:01:12 +0800

    finish removeDuplicatesFromSortedList

---
+++ Algorithms/cpp/removeDuplicatesFromSortedList/removeDuplicatesFromSortedList.swift
@@ -0,0 +1,69 @@
+#!/usr/bin/swift
+
+class ListNode {
+    var val: Int
+    var next: ListNode?
+    
+    init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+func genList(_ list: [Int]) -> ListNode? {
+    if list.count == 0 {
+        return nil
+    }
+    let head: ListNode = ListNode(list[0])
+    var node: ListNode = head
+    for idx in (1...list.count-1) {
+        let newNode = ListNode(list[idx])
+        node.next = newNode
+        node = newNode
+    }
+    return head
+}
+
+func printNodeList(_ node: ListNode) {
+    var arr: [Int] = []
+    var node: ListNode? = node
+    while (node != nil) {
+        arr.append(node!.val)
+        node = node?.next
+    }
+    print("\(arr)")
+}
+
+func deleteDupicates(_ head: ListNode) -> ListNode? {
+    let fake = ListNode(-1)
+    fake.next = head
+    var preNode = fake
+    var node: ListNode? = head
+    
+    while (node != nil && node!.next != nil) {
+        var isDup = false
+        if node!.val == node!.next!.val {
+            isDup = true
+            node!.next = node!.next!.next
+        }
+        if node!.val != (node!.next ?? ListNode(-1)).val{
+            if isDup {
+                preNode.next = node!.next
+            } else {
+                preNode = node!
+            }
+            node = node!.next
+        }
+    }
+    return fake.next
+}
+
+func main() {
+    let data = [1,1,2,3,3,3,3,4,4,4,5,5,6,6]
+    let node = genList(data)
+    printNodeList(node!)
+    let newNode = deleteDupicates(node!)
+    printNodeList(newNode!)
+}
+
+main()
--- notes/products/leetcode/leetcode-api.md
+++ notes/products/leetcode/leetcode-api.md
@@ -6,3 +6,39 @@ https://leetcode.com/graphql
 
 3.login
 https://leetcode.com/account/login/
+
+
+4.runcode
+https://leetcode.com/playground/api/runcode
+```
+@运行代码API
+@method: post
+@request_param:
+data_input: "[1, 2, 3]" #参数
+lang: "cpp" #语言
+typed_code: "int main() {
+    cout << "My test code" << endl;
+    return 0;
+}" # 代码
+
+@response: #json格式数据
+interpret_id: #任务ID
+test_case: #
+```
+
+5.检查运行结果
+```
+https://leetcode.com/submissions/detail/interpret_1569206126.2464511/check/
+
+API: https://leetcode.com/submissions/detail/{submission_id}/check/
+@method: GET
+@response: #json格式数据
+code_output: #代码输出结果
+state: #运行状态 SUCCESS 成功,STARTED 运行中,
+status_runtime: #运行时间
+run_success: true 是否成功
+submission_id: 任务ID
+...
+```
+
+
