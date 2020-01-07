42ef64523392fadaefb44574480efac1e7b9c9d5
shuai.liu <shuai.liu@transsnet.com>
2020-01-07 17:43:18 +0800

    algorithms

---
+++ Algorithms/cpp/160_getIntersectionNode/getIntersectionNode.cpp
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    return headA;
+}
+
+int main() {
+    printf("abc\n");
+    return 0;
+}
