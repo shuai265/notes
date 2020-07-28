b8e11374b6d583ece9c6add3f2315c9d1af67c0d
shuai.liu <shuai265@qq.com>
2020-07-28 18:55:17 +0800

    add new 026

---
+++ Algorithms/cpp/026_removeDuplicatesFromSortedArray/026_removeDuplicatesFromSortedArray.swift
@@ -0,0 +1,26 @@
+// Source : https://oj.leetcode.com/problems/remove-duplicates-from-sorted-array/
+// Author : Hao Chen
+// Date   : 2014-06-22
+// https://github.com/haoel/leetcode/blob/master/algorithms/cpp/removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.cpp
+// https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+
+/********************************************************************************** 
+* 
+* Given a sorted array, remove the duplicates in place such that each element appear 
+* only once and return the new length.
+* 
+* Do not allocate extra space for another array, you must do this in place with constant memory.
+* 
+* For example,
+* Given input array A = [1,1,2],
+* 
+* Your function should return length = 2, and A is now [1,2].
+* 
+*               
+**********************************************************************************/
+
+class Solution {
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+        
+    }
+}
