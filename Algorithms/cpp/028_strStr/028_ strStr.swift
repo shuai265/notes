e827b700507933b0c4a873e67ebc5e4b124d4da1
shuai.liu <shuai265@qq.com>
2020-07-29 19:28:00 +0800

    add new alth

---
+++ Algorithms/cpp/028_strStr/028_ strStr.swift
@@ -0,0 +1,12 @@
+
+// https://leetcode-cn.com/problems/implement-strstr/
+
+// Source : https://oj.leetcode.com/problems/implement-strstr/
+/********************************************************************************** 
+* 
+* Implement strStr().
+* 
+* Returns a pointer to the first occurrence of needle in haystack, or null if needle is not part of haystack.
+* 
+*               
+**********************************************************************************/
