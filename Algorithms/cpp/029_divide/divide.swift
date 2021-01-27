f2d68ba9daa3118478cb2e4280e106a32d2467e7
shuai.liu <shuai265@qq.com>
2021-01-27 13:01:31 +0800

    finish 029

--- Algorithms/cpp/029_divide/divide.swift
+++ Algorithms/cpp/029_divide/divide.swift
@@ -2,9 +2,17 @@
 
 
 /**
+ * https://leetcode-cn.com/problems/divide-two-integers/
+ * 
  * 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
  * 返回被除数 dividend 除以除数 divisor 得到的商。
  * 整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) = -2
+ * 提示：
+
+被除数和除数均为 32 位有符号整数。
+除数不为 0。
+假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。本题中，如果除法结果溢出，则返回 231 − 1。
+
  */
 
 
@@ -50,15 +58,29 @@ class Solution {
                 res += idx
             }
         }
-        return isNeg ? -res : res
+
+        res = isNeg ? -res : res
+        if res > Int32.max || res < Int32.min {
+            return Int(Int32.max)
+        }
+
+        return res
     }
 }
 
 //  let dividend = 10
- let dividend = -15
+//  let dividend = -15
  
 //  let divisor = 3
- let divisor = -2
- 
+//  let divisor = -2
+
+// print(Int32.max)
+// print(Int32.min)
+
+ let dividend = -2147483648
+ let divisor = -1
+// let divisor = 1
+
+
  let ans = Solution().divide(dividend, divisor)
  print("ans = \(ans)")
