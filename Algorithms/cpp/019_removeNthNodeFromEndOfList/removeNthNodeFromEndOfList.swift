050b8cb741d1aaba635fffef2d154275015e506e
shuai.liu <shuai265@qq.com>
2021-02-05 13:22:58 +0800

    update 019

--- Algorithms/cpp/019_removeNthNodeFromEndOfList/removeNthNodeFromEndOfList.swift
+++ Algorithms/cpp/019_removeNthNodeFromEndOfList/removeNthNodeFromEndOfList.swift
@@ -29,14 +29,44 @@
  * }
  */
 
+// public class ListNode {
+//     public var val: Int
+//     public var next: ListNode?
+//     public init(_ val: Int) {
+//         self.val = val
+//         self.next = nil
+//     }
+//  }
+
 public class ListNode {
     public var val: Int
     public var next: ListNode?
-    public init(_ val: Int) {
-        self.val = val
-        self.next = nil
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+class Solution2 {
+    func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        guard let _ = head else {
+            return head
+        }
+        let pre = ListNode(-1, head!)
+        var nodes = [pre]
+
+        var node = head
+        while node != nil {
+            nodes.append(node!)
+            node = node!.next
+        }
+        // print("nodes = \(nodes)")
+        let target = nodes[nodes.count-n]
+        let preNode = nodes[nodes.count-n-1] // 注意边界
+        preNode.next = target.next
+        return pre.next
     }
- }
+}
+
 
 class Solution {
     func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
@@ -63,3 +93,38 @@ class Solution {
         return preHead.next
     }
 }
+
+func listToNode(_ list: [Int]) -> ListNode? {
+    guard list.count > 0 else {
+        return nil
+    }
+    let fake = ListNode(-1)
+    var node = fake
+    for num in list {
+        let newNode = ListNode(num)
+        node.next = newNode
+        node = newNode
+    }
+    return fake.next
+}
+
+func printNode(_ head: ListNode?) {
+    var nums = [Int]()
+    var node = head
+    while node != nil {
+        nums.append(node!.val)
+        node = node!.next
+    }
+    print(nums)
+}
+
+
+// let nums = [1,2,3,4,5]
+// let n = 1
+let nums = [1]
+let n = 1
+
+let head = listToNode(nums)
+
+var ans = Solution2().removeNthFromEnd(head, n)
+printNode(ans)
